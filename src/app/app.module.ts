7bce7fc9ac75b0ed772a29bacbcc2439e3a00fc9
TheNunHahey <127714380+TheNunHahey@users.noreply.github.com>
2023-03-15 16:41:03 +0700

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,23 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    HomeComponent,
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, VERSION, OnInit } from '@angular/core';
+import { products } from '../products';
+import { Observable } from 'rxjs';
+import liff from '@line/liff';
+
+type UnPromise<T> = T extends Promise<infer X> ? X : T;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  os: ReturnType<typeof liff.getOS>;
+  profile: UnPromise<ReturnType<typeof liff.getProfile>>;
+  onClick(event?: MouseEvent) {
+    if (event) {
+      event.stopPropagation();
+    }
+
+    liff
+      .init({ liffId: '1657421042-ekawW2jw' })
+      .then(() => {
+        this.os = liff.getOS();
+        if (liff.isLoggedIn()) {
+          liff
+            .getProfile()
+            .then((profile) => {
+              this.profile = profile;
+            })
+            .catch(console.error);
+        } else {
+          liff.login();
+        }
+      })
+      .catch(console.error);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
