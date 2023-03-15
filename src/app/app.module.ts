62c6f61c21df1e7f23d964ae2f2715679f742428
TheNunHahey <127714380+TheNunHahey@users.noreply.github.com>
2023-03-15 22:34:40 +0700

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-//import { AppointmentComponent } from './appointment/appointment.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     RouterModule.forRoot([{ path: '', component: HomeComponent }]),
   ],
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, AppointmentComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
